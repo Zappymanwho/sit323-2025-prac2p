2fdfb1827176ea0f7edb757166d089e25f86de95
Zappymanwho <zaipesto@gmail.com>
2025-03-13 19:06:05 +1100

    hello world web page with express

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const res = require('express/lib/response');
+const path = require('path'); // Add this line to require the 'path' module
 const app = express();
+
+//adding two numbers here
 const addTwoNumber=(n1,n2) => {
     return n1+n2
 }
+// This is for index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
 
-//here we respond with hello world when a GET response is made
+//here we respond with sum of two numbers here when a GET response is made
 app.get('/addTwoNumber',(req,res)=>{
     const n1=parseInt(req.query.n1);
     const n2=parseInt(req.query.n2);
     const result = addTwoNumber(n1,n2);
     res.json({statuscode:200, data: result})
 });
+
 console.log(addTwoNumber((19,12)));
-const port=3040;
+const port=3080;
 app.listen(port,()=>{
     console.log("Im listening to the port"+port)
 })
 
-console.log(Week-1)
+console.log("Week1");
+
+console.log(addTwoNumber(19,12));
 
 var async = function()
 {
